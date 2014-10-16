674598d8b12d0d0f6c6a1c7c120dab235ce76a9a
Chirag Vora <cb.vora@yahoo.com>
2014-10-16 08:54:29 +0530

    Fine

---
+++ src/main.js
@@ -0,0 +1,19 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Chirag Vora";
+var letterColors = [red, orange, green]
+
+if(15 < 5)
+    {
+        var bubbleShape = "square";
+    }
+else // bka !! it's not required 
+    {
+        var bubbleShape = "circle";
+    }
+drawName(myName, letterColors);
+bounceBubbles()
